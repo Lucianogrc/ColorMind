8b87a1ac59b6d8169ccde73cdac30ec0cebfc1d3
JoseAlfredo0201 <A01645391@tec.mx>
2025-05-07 23:59:16 -0600

    feat: add color palette generator logic

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 from PIL import Image
+import os
 from utils.palette_generator import build_palette
-
-
+from utils.color_analysis import extract_dominant_color
+from utils.color_analysis import rgb_to_hex
+from utils.palette_generator import generate_palette
 
 st.set_page_config(page_title="ColorMind", layout="centered")
 
@@ -11,6 +13,8 @@ st.subheader("Upload a logo and get a custom color palette")
 
 uploaded_file = st.file_uploader("Upload your logo", type=["png", "jpg", "jpeg"])
 concept = st.selectbox("Select a design concept", ["modern", "elegant", "youthful"])
+
+
 
 if uploaded_file:
     image = Image.open(uploaded_file)
@@ -31,3 +35,14 @@ if uploaded_file:
             unsafe_allow_html=True
         )
         st.code(hex_color)
+
+    # Mostrar color principal detectado
+    dominant_rgb = extract_dominant_color("temp_logo.png")
+    dominant_hex = rgb_to_hex(dominant_rgb)
+
+    st.subheader("🎯 Dominant Color Detected")
+    st.markdown(
+        f"<div style='background-color:{dominant_hex}; height:40px;'></div>",
+        unsafe_allow_html=True
+    )
+    st.code(dominant_hex)
